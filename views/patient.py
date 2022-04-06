58be8fe3b24d5b7db49d3db43e64d1f66d82d7c0
anatoli <Anatoli.Aheichyk@itechart-group.com>
2022-04-06 12:27:32 +0300

    update patient view

--- views/patient.py
+++ views/patient.py
@@ -1,9 +1,10 @@
 from hashlib import new
 from flask import Blueprint, jsonify, request, Response
 from config import db
+from models.user import User
 from models.patient import Patient, Address
 from schemas import patient_schema, patients_schema,\
-                    address_schema
+                    address_schema, addresses_schema
 from services.auth import token_required, get_user_from_request
 from services.services import get_object_by_id, is_equal_user_from_request_with_user_db
 
@@ -15,108 +16,128 @@ def get_patients():
     return jsonify(patients_schema.dump(Patient.query.all()))
 
 
+
+@patient_view.route("/user/<id>/patient", methods=["Get"])
+def get_patient(id):
+    user = get_object_by_id(Model=User, id=id)
+    return Response(patient_schema.dumps(user.patient), status=200, mimetype='application/json')
+
+
 @token_required
-@patient_view.route("/patients", methods=["Post"])
-def create_patient():
+@patient_view.route("/user/<id>/patient", methods=["Post"])
+def create_patient(id):
     user = get_user_from_request(request)
+    if not user.id == int(id):
+        return Response(status=403)
+    if user.patient:
+        return Response("user already have patient", status=400)
     patient_data = patient_schema.load(request.json)
     new_patient = Patient(**patient_data)
-    new_patient.user = user
+    user.patient = new_patient
     db.session.add(new_patient)
     db.session.commit()
     return Response(patient_schema.dumps(new_patient), status=201, mimetype='application/json')
 
 
-
-@patient_view.route("/patient/<patient_id>", methods=["Get"])
-def get_patient_by_id(patient_id):
-    patient = get_object_by_id(Model=Patient, id=patient_id)
-    return Response(patient_schema.dumps(patient), status=200, mimetype='application/json')
-
 @token_required
-@patient_view.route("/patient/<patient_id>", methods=["Put"])
-def update_patient(patient_id):
-    user =  get_user_from_request(request)
-    patient = get_object_by_id(Model=Patient, id=patient_id)
-    if not is_equal_user_from_request_with_user_db(request_user=user, db_user=patient.user):
-        return Response(status=403)    
-    patient_data = patient_schema.load(request.json)
-    db.session.query(Patient).filter(Patient.id == patient_id).update(patient_data)
-    db.session.commit()
-    
-    return Response(status=200)
-
-@token_required
-@patient_view.route("/patient/<patient_id>", methods=["Delete"])
-def delete_patient(patient_id):
-    user =  get_user_from_request(request)
-    patient = get_object_by_id(Model=Patient, id=patient_id)
-    if not is_equal_user_from_request_with_user_db(request_user=user, db_user=patient.user):
+@patient_view.route("/user/<id>/patient", methods=["Put"])
+def update_patient(id):
+    user = get_user_from_request(request)
+    if not user.id == int(id):
         return Response(status=403)
-    
-    db.session.delete(Patient.query.filter_by(id=patient_id).first())
+    if not user.patient:
+        return Response("user dont have patient", status=400)
+    patient_data = patient_schema.load(request.json)
+    Patient.query.filter_by(id=user.patient.id).update(patient_data)
     db.session.commit()
+    return Response(
+        patient_schema.dumps(Patient.query.filter_by(id=user.patient.id).one()),
+        status=201,
+        mimetype='application/json')
+    
 
+@token_required
+@patient_view.route("/user/<id>/patient", methods=["Delete"])
+def delete_patient(id):
+    user = get_user_from_request(request)
+    if not user.id == int(id):
+        return Response(status=403)
+    if not user.patient:
+        return Response("user dont have patient", status=400)
+    db.session.delete(Patient.query.filter_by(id=user.patient.id).one())
+    db.session.commit()
     return Response(status=204)
 
 
-@patient_view.route("/patient/<patient_id>/address", methods=["Get"])
-def get_address(patient_id):
-    patient = get_object_by_id(Model=Patient, id=patient_id)
-    address = patient.address
-    return Response(address_schema.dumps(address), status=200, mimetype='application/json')
+@patient_view.route("/user/<id>/patient/address", methods=["Get"])
+def get_address(id):
+    user = get_object_by_id(Model=User, id=id)
+    if not user.patient:
+        return Response("user dont have patient", status=400)
+    if not user.patient.address:
+        import pdb; pdb.set_trace()
+        return Response({}, status=200, mimetype='application/json')
+    return Response(
+        addresses_schema.dumps(Address.query.filter_by(id=user.patient.adddres).one_or_none()),
+        status=200,
+        mimetype='application/json'
+        )
+
 
 @token_required
-@patient_view.route("/patient/<patient_id>/address", methods=["Post"])
-def create_address(patient_id):
-    user =  get_user_from_request(request)
-    patient = get_object_by_id(Model=Patient, id=patient_id)
-    if not is_equal_user_from_request_with_user_db(request_user=user, db_user=patient.user):
+@patient_view.route("/user/<id>/patient/address", methods=["Post"])
+def create_address(id):
+    user = get_user_from_request(request)
+    if not user.id == int(id):
         return Response(status=403)
-    
-    if patient.address:
+    if not user.patient:
+        return Response("user dont have patient", status=400)
+    if user.patient.address:
         return Response("patient already have address", status=400)
     
     address_data = address_schema.load(request.json)
     new_address = Address(**address_data)
-    patient.address =  new_address
+    user.patient.address = new_address
     db.session.add(new_address)
     db.session.commit()
+    return Response(
+        address_schema.dumps(new_address),
+        status=201,
+        mimetype='application/json')
 
-    return Response(address_schema.dumps(new_address), status=201, mimetype='application/json')
 
 @token_required
-@patient_view.route("/patient/<patient_id>/address", methods=["Put"])
-def update_address(patient_id):
-    user =  get_user_from_request(request)
-    patient = get_object_by_id(Model=Patient, id=patient_id)
-    
-    if not is_equal_user_from_request_with_user_db(request_user=user, db_user=patient.user):
+@patient_view.route("/user/<id>/patient/address", methods=["Put"])
+def update_address(id):
+    user = get_user_from_request(request)
+    if not user.id == int(id):
         return Response(status=403)
-    
-    if not patient.address:
+    if not user.patient:
+        return Response("user dont have patient", status=400)
+    if not user.patient.address:
         return Response("patient dont have address", status=400)
-
+    
     address_data = address_schema.load(request.json)
-    Address.query.filter_by(id=patient.address.id).update(address_data)
-    
+    Address.query.filter_by(id=user.patient.address.id).update(address_data)
     db.session.commit()
+    return Response(
+        address_schema.dumps(Address.query.filter_by(id=user.patient.address.id).one()),
+        status=200,
+        mimetype='application/json')
 
-    return Response(status=200)
 
 @token_required
-@patient_view.route("/patient/<patient_id>/address", methods=["Delete"])
-def delete_address(patient_id):
-    user =  get_user_from_request(request)
-    patient = get_object_by_id(Model=Patient, id=patient_id)
-
-    if not is_equal_user_from_request_with_user_db(request_user=user, db_user=patient.user):
+@patient_view.route("/user/<id>/patient/address", methods=["Delete"])
+def delete_address(id):
+    user = get_user_from_request(request)
+    if not user.id == int(id):
         return Response(status=403)
-    
-    if not patient.address:
+    if not user.patient:
+        return Response("user dont have patient", status=400)
+    if not user.patient.address:
         return Response("patient dont have address", status=400)
+    
 
-    db.session.delete(Address.query.filter_by(id=patient.address.id).first())
+    db.session.delete(Address.query.filter_by(id=user.patient.address.id).one())
     db.session.commit()
-
     return Response(status=204)
